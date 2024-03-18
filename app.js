35fab3983424921db3649696adcedab00aa0062f
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-03-18 17:12:59 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const url = `https://fakestoreapi.com/products`;
+async function getProducts(){
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error('Fetch did not work!')
+        }
+
+        const data = await response.json();
+        const container = document.getElementById('container');
+        data.forEach(item => {
+            const divEl = document.createElement('div');
+            divEl.classList.add('product');
+            divEl.innerHTML+=`
+                <img src="${item.image}" alt="${item.title}" class="product-img">
+                <span class="product-title">${item.title}</span>
+                <span class="product-price">UShs. ${item.price*3700}</span>
+                <button class="add-to-cart-btn" onclick="alert('Added successfully')">Add to cart</button>
+            `
+            container.appendChild(divEl);
+
+        });
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+getProducts();
